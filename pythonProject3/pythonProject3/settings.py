d6aa4f794a9bbbc2bb58357495fc44f77613344e
srijith13 <srijith31@gmail.com>
2022-05-28 12:46:06 +0530

    I have added login and register page

--- pythonProject3/pythonProject3/settings.py
+++ pythonProject3/pythonProject3/settings.py
@@ -81,6 +81,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'facerecognition.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -118,7 +119,4 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
